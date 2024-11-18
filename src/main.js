ad50fc927a9740cb85c265bbfb0c52552a4f8940
Nandani <singlanandani65@gmail.com>
2024-11-18 16:46:52 -0500

    created the menuscene.js and imported in the main file

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import MenuScene from './MenuScene.js';
 import AlienInvasion from './AlienInvasion.js';
 import GameOver from './GameOver.js';
 
@@ -7,9 +8,9 @@ const config = {
   height: 600,
   physics: {
     default: 'arcade',
-    arcade: { debug: false }
+    arcade: { debug: false },
   },
-  scene: [AlienInvasion, GameOver]  // Define your scenes here
+  scene: [MenuScene, AlienInvasion, GameOver], // Both scenes are used
 };
 
 const game = new Phaser.Game(config);
